145fde7f15438c70aa28fa04ad3a28b19cb89d8b
layfredrc <47161258+layfredrc@users.noreply.github.com>
2024-04-05 01:29:15 +0200

    upgrade strapi 4.3.3

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'c3aeaa71045f37bf7f1e2db66bc1294b'),
-  },  
+    secret: env('ADMIN_JWT_SECRET', 'ab763252-fa1b-4474-ae90-4dec048d9fe4'),
+  },
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'wruCdFcj6cSgKgvWVqgXww=='),
+    salt: env('API_TOKEN_SALT', '6a61c01c3779d5ad7edc3a95a5d34a06'),
   },
 });
